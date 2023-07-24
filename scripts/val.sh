25319bb6e16611c800b5acb3c149b7ab0e30ef6c
chenxingyu <chenxingyusean@163.com>
2023-07-24 16:39:59 +0800

    update python path

--- scripts/val.sh
+++ scripts/val.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 /cpfs/user/chenxingyu/.conda/envs/pt111_mnt/bin/python calc_metrics.py \
+CUDA_VISIBLE_DEVICES=0 python calc_metrics.py \
     --cfg='configs/ffhq512_3d.yaml' \
     gpus 1
